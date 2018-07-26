368096c3b4960fdafa2326d67d95e19bbafe73d1
DIMA <tabachniy7@gmail.com>
2018-07-27 00:01:58 +0300

    1

---
+++ sum.ts
@@ -0,0 +1,21 @@
+export class Car {
+  amountOfFuel: number;
+  fuelСonsumption: number;
+  maxDistance: number;
+
+    constructor(carAmountOfFuel: number,
+      carFuelСonsumption: number) {
+        this.amountOfFuel = carAmountOfFuel;
+        this.fuelСonsumption = carFuelСonsumption;
+      }
+
+      distanceСalculation(): number {
+        return (this.amountOfFuel / this.fuelСonsumption)*100;
+    }
+}
+
+export class Bentley extends Car {
+  }
+
+export class Zhiguli extends Car {
+  }
